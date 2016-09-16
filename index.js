5f2865102dc86c0f625de4a4ec4088c09a0ad457
Marine Boudeau <m14868@gmail.com>
2016-09-16 10:54:58 +0000

    Done.

--- index.js
+++ index.js
@@ -19,5 +19,13 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll(".ranked-list")
   
+  for (var i=0, l = rankedLists.length; i < l; i++){
+    var listItem = rankedLists[i].children
+    
+    for (var j=0, m=listItem.length; j < m; j++) {
+      listItem[j].innerHTML = parseInt(listItem[j].innerHTML) + n      
+    }
+  }  
 }
